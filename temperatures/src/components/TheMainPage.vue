154933c092bfcf9af09e86834ccab9ee28e1d938
Laura DAddese <laura.daddese@heig-vd.ch>
2023-04-17 10:42:54 +0200

    first commit

---
+++ temperatures/src/components/TheMainPage.vue
@@ -0,0 +1,43 @@
+<script setup>
+import BaseMessage from "./base/BaseMessage.vue";
+import BaseInputNumber from "./base/BaseInputNumber.vue";
+import { computed, ref } from "vue";
+
+const kelvin = ref(0);
+const celcius = computed({
+  get() {
+    return kelvin.value - 273.15;
+  },
+  set(value) {
+    kelvin.value = value + 273.15;
+  },
+});
+const fahrenheit = computed({
+    get(){
+        return kelvin.value * 9/5 - 459.67;
+    },
+    set(value){
+       kelvin.value = (value + 459.67) * 5/9;
+    },
+});
+</script>
+
+<template>
+  <base-message data-role="error"> Error: plz try again later </base-message>
+  <base-message data-role="warning">
+    Application will be in maintenance ...
+  </base-message>
+  <base-message data-role="test"> Test message </base-message>
+  <!-- <base-input
+    label="Kelvin"
+    unit="K"
+    :modelValue="kelvin"
+    @update:modelValue="changeKelvin($event)"
+  /> -->
+  <base-input-number v-model="kelvin" label="Kelvin" unit="K" />
+  <base-input-number label="celcius" unit="°C" v-model="celcius" />
+  <base-input-number label="fahrenheit" unit="°F" v-model="fahrenheit" />
+  <p>Hello</p>
+</template>
+
+<style scoped></style>
---
+++ temperatures/src/components/base/BaseMessage.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  dataRole : {
+    type: String,
+    required: false,
+    default: 'info'
+  }
+})
+</script>
+
+<template>
+  <div>
+    <p :class="dataRole">
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+  p {
+    padding: 0.5rem;
+  }
+  .info {
+    background-color: #2196F3;
+    color: #fff;
+  }
+
+  .warning {
+    background-color: #FFC107;
+    color: #000;
+  }
+
+  .error {
+    background-color: #F44336;
+    color: #fff;
+  }
+
+  .test {
+    background-color: #9C27B0;
+    color: #fff;
+  }
+
+
+</style>
