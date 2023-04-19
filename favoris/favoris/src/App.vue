9a2f9793e29c6eebb994490295dc33997e250b73
Laura DAddese <laura.daddese@heig-vd.ch>
2023-04-19 13:30:29 +0200

    url cliquables et ajout d'ID pour favoris

--- favoris/favoris/src/App.vue
+++ favoris/favoris/src/App.vue
@@ -1,11 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from '@vue/reactivity';
+import AjoutFavoris from './components/AjoutFavoris.vue'
+
+const tabFavoris = ref([]);
+
+//ajouter un élément au tableau
+const ajoutFavoris = (fav) => {
+  //ajouter un id
+  tabFavoris.value.push(fav);
+  console.log(tabFavoris);
+}
+
+ajoutFavoris({id:Date.now(), nom:"gaps", url:"https://gaps.heig-vd.ch/consultation/controlescontinus/consultation.php?idst=17749", categories:["cours", "agenda"]});
+ajoutFavoris({id:Date.now(), nom:"doc vue", url:"https://vuejs.org/guide/introduction.html#what-is-vue", categories:["cours"]});
+
 </script>
 
 <template>
-<p>test</p>
+  <div>
+    <h1>Liste favoris</h1>
+    <ul>
+      <p v-for="favoris of tabFavoris">
+        {{ favoris.nom }}
+        <a :href="favoris.url">url</a>
+      </p>
+    </ul>
+  </div>
+  <div>
+    <h1>Ajouter un favoris</h1>
+    <AjoutFavoris  @add:fav="ajoutFavoris($event)"/>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ favoris/favoris/src/components/AjoutFavoris.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({});
+
+const emits = defineEmits(["add:fav"]);
+
+const id = ref("");
+const nom = ref("");
+const url = ref("");
+const categories = ref("");
+
+function ajoutFavoris() {
+    emits("add:fav", { nom: nom.value, url: url.value, categories: categories.value });
+}
+</script>
+
+<template>
+  <!--le v-model sert à récupérer l'input-->
+  <label>Nom </label>
+  <input type="text" v-model="nom" />
+  <br>
+  <label>URL </label>
+  <input type="url" v-model="url" />
+  <br>
+  <label>Catégorie </label>
+  <input type="text" v-model="categories" />
+  <br>
+  <button @click="ajoutFavoris">Ajouter</button>
+</template>
+
+<style scoped></style>
